bda3689dc620783c47fe4eefc69ce623bbc8cc42
matteoindino <matteo.indino@gmail.com>
2025-06-30 00:29:53 +0200

    Added environment variables for Facebook App ID and Secret

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+from dotenv import load_dotenv
 import os
+
+# Carica le variabili d'ambiente dal file .env
+load_dotenv()
+
+# Recupera le variabili d'ambiente
+FACEBOOK_APP_ID = os.getenv("FACEBOOK_APP_ID")
+FACEBOOK_APP_SECRET = os.getenv("FACEBOOK_APP_SECRET")
 
 class Settings:
     MEDIA_UPLOAD_DIR = os.environ.get("MEDIA_UPLOAD_DIR", "media_uploads")
